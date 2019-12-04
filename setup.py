4b34f46357a714f8ff3ae57bb9c08722104c9198
Pedro Meirelles <pedro.meirelles@bizbank.com.br>
2019-12-04 18:13:06 -0300

    removing comments, adding requirements and some description

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from distutils.core import setup
 setup(
-  name='woe-monotonic-binning',         # How you named your package folder (MyLib)
-  packages=['woe-monotonic-binning'],   # Chose the same as "name"
-  version='0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description='Optimal binning algorithm and function to apply on a pandas DataFrame',   # Give a short description about your library
-  author='PEDRO HENRIQUE BAUMGRATZ MEIRELLES',                   # Type in your name
-  author_email='pedrohbm@poli.ufrj.br',      # Type in your E-Mail
-  url='https://github.com/PedroHBM/woe-monotonic-binning',   # Provide either the link to your github or to your website
-  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  name='woe-monotonic-binning',
+  packages=['woe-monotonic-binning'],
+  version='0.1',
+  license='MIT',
+  description='Optimal binning algorithm and function to apply on a pandas DataFrame',
+  author='PEDRO HENRIQUE BAUMGRATZ MEIRELLES',
+  author_email='pedrohbm@poli.ufrj.br',
+  url='https://github.com/PedroHBM/woe-monotonic-binning',
+  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords=['WOE', 'BINNING', 'IV', 'LOGISTIC', 'REGRESSION'],
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'pandas',
+          'numpy',
+          'scipy',
+          'tqdm',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Data Scientists',      
+    'Topic :: Data Science :: Analytics :: Statistics',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
