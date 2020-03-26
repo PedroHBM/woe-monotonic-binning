ba27fc03eb2bff35391dc27008d72e6926b07f6f
Pedro Meirelles <pedro.meirelles@bizbank.com.br>
2020-03-26 19:44:38 -0300

    missing function import

--- woe_monotonic_binning/__init__.py
+++ woe_monotonic_binning/__init__.py
@@ -1,1 +1,1 @@
-from woe_monotonic_binning.binning import batch_woe_binning, woe_binning, apply_bins, merge_bins
+from woe_monotonic_binning.binning import batch_woe_binning, woe_binning, apply_bins, merge_bins, woe_binning_sep
